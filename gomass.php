19a17c8fb27001379a3a22e9f2357768aa2a4446
Greytak <simon.dad@gmail.com>
2015-01-12 16:44:26 +0100

    begin changes to manage several users and parties

--- gomass.php
+++ gomass.php
@@ -1,84 +1,112 @@
-<?php
+<?
 //-----------------------------------------------------------
 //-----------------------------------------------------------
-global $grillex, $grilley, $onearray, $therequete;
+global $grillex, $grilley, $therequete;
+global $usersarray, $partiesarray, $theparty;
 $grillex = 8;  $grilley = 2;
-//-----------------------------------------------------------
 $str_json = file_get_contents('php://input');
 $therequete= json_decode($str_json, TRUE);
-//-----------------------------------------------------------
-if (isset($therequete["command"]))
-  if ($therequete["command"]=="resetgame") {
-    unlink("onearray.json");
-    unset($therequete["command"]);
-  }
-//-----------------------------------------------------------
-load_game();
+load_users();
 auth_user();
+list_parties();
+if (isset($therequete["command"]))
+  call_user_func($therequete["command"]);
 //-----------------------------------------------------------
-if ($onearray["yourturn"]==1)
-  if (isset($therequete["command"]))
-    call_user_func($therequete["command"]);
-//-----------------------------------------------------------
+$onearray= array();
+$onearray["partiesarray"]= $partiesarray;
 $onearray_json= json_encode($onearray);
 file_put_contents("onearray.json", $onearray_json);
 header('Content-Type: application/json');
 echo $onearray_json;
 //-----------------------------------------------------------
 //-----------------------------------------------------------
-function auth_user() {
-  global $onearray;
-  if (isset($_COOKIE["idid"]))
-    if (isset($onearray["users"][$_COOKIE["idid"]])) {
-      if ($_COOKIE["idid"]==$onearray["turnid"])
-        $onearray["yourturn"]= 1;
-      else
-        $onearray["yourturn"]= 0;
-      return;
-    }
-  //file_put_contents ( "cookie.log" , $_COOKIE["idid"]);
-  if ($onearray["users"].length>1) unset($onearray["users"]);
-  $uniqidcook= uniqid('', true);
-  $onearray["users"][$uniqidcook]= "date";
-  if (!isset($onearray["turnid"])) {
-    $onearray["turnid"]= $uniqidcook;
-    $onearray["yourturn"]= 1;
-  } else
-    $onearray["yourturn"]= 0;
-  setcookie("idid", $uniqidcook);
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+function createparty() {
+  global $theparty;
+  $theparty= array();
+  $theparty["player1"]= $usersarray[$_COOKIE["idid"]]
+  $uniqidparty= uniqid('', true);
+  $theparty["id"]= $uniqidparty;
+  $theparty_json= json_encode($theparty);
+  file_put_contents("parties/"$uniqidparty.".json", $theparty_json);
 }
 //-----------------------------------------------------------
-function load_game() {
-  global $onearray, $grillex, $grilley;
-  if ($onearray = json_decode(file_get_contents("onearray.json"), TRUE)) {
+function list_parties() {
+  global $partiesarray;
+  if ($partiesarray = json_decode(file_get_contents("partiesarray.json"), TRUE)) {
   } else {
-    for ($i = 0; $i < $grillex; $i++) {
-      for ($j = 0; $j < $grilley; $j++) {
-        $grille[$i][$j]['carte']= 0;
-      }
+      $partiesarray = array();
     }
-    $grille[0][0]['carte']= 1;
-    $grille[1][1]['carte']= 1;
-    $grille[2][1]['carte']= 1;
-    $onearray = array(1=>'case11', 2=>'case12', 3=>'case13', 'd' => 4, 'grille' => $grille);
+}
+//-----------------------------------------------------------
+function auth_user() {
+  global $usersarray;
+  if (isset($_COOKIE["idid"]))
+  if (isset($usersarray[$_COOKIE["idid"]])) {
+    //refresh date ?
+    return;
   }
+  //file_put_contents ( "cookie.log" , $_COOKIE["idid"]);
+  $uniqidcook= uniqid('', true);
+  $usersarray[$uniqidcook]= "date";
+  setcookie("idid", $uniqidcook);
+  $usersarray_json= json_encode($usersarray);
+  file_put_contents("usersarray.json", $usersarray_json);
 }
 //-----------------------------------------------------------
-function gomove() {
-  global $onearray, $therequete;
-  if (isset($therequete["srcx"]) && isset($therequete["srcy"]) && isset($therequete["desx"]) && isset($therequete["desy"])) {
-    $onearray['grille'][intval($therequete["srcx"])][intval($therequete["srcy"])]['carte']= 0;
-    $onearray['grille'][intval($therequete["desx"])][intval($therequete["desy"])]['carte']= 1;
-    //$onearray['command']='moveok';
-  }
+function load_users() {
+  global $usersarray;
+  if ($usersarray = json_decode(file_get_contents("usersarray.json"), TRUE)) {
+  } else {
+      $usersarray = array();
+    }
 }
 //-----------------------------------------------------------
-function endturn() {
-  global $onearray;//, $therequete;
-  $onearray["yourturn"]= 0;
-  foreach ($onearray["users"] as $key => $value)
-    if ($key!=$_COOKIE["idid"]) $onearray["turnid"]= $key;
-  //$onearray['command']='endturnok';
-}
 //-----------------------------------------------------------
+//-----------------------------------------------------------
+
+/*
+Gomass :
+--------------------------------------------
+setTimeout ("goajaxrequete()", 750);
+click destination => command move
+click end turn => command endturn
+click reset => command resetgame
+
+--------------------------------------------
+liste des parties -> joindre, créer partie -> créer
+
+--------------------------------------------
+Users : liste d'IDs
+Parties : ID_partie, ID_joueur1, ID_joueur2
+Partie : ID_partie, ID_joueur1, ID_joueur2, grille
+
+--------------------------------------------
+- load_users "usersarray.json"
+- authenticate user
+- appel vide : liste parties
+- joindre partie (ID_partie) : load/send partie
+- action partie (ID_partie, src, dest) : load partie , action, save partie, send partie
+- créer partie : créer partie, load/send partie
+
+--------------------------------------------
+authenticate user :
+  foreach Users :
+    if ID_joueur==ID_cookie OK else
+      setcookie
+      save_Users
+
+liste parties : ID_joueur1==null OR ID_joueur1==ID_cookie OR ID_joueur2==null OR ID_joueur2==ID_cookie
+
+load partie(ID_partie) :
+  foreach partie :
+    if ID_partie==ID_partie if ID_joueur1==ID_cookie OR ID_joueur2==ID_cookie
+      jsondecode
+
+save partie : jsonencode
+
+send partie : jsonencode
+--------------------------------------------
+*/
 ?>
