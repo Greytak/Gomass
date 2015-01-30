290ef9fd4ecf12081c4f4e028a9b61364fa801b0
cap2fosse <cap2fosse@gmail.com>
2015-01-30 23:26:10 +0100

    Les plateau peuvent être positionnés dans la page. Mise en place des requetes ajax sur le mouvement des cartes.

--- pomme.js
+++ pomme.js
@@ -1,34 +1,39 @@
 /* 
  *Plateau object
 */
-function Plateau(largeur, hauteur) {
+function Plateau(largeur, hauteur, posx, posy) {
   this.largeur = largeur;
   this.hauteur = hauteur;
   this.cases = new Array();
+  // create all cases and cartes
   this.create = function() {
     id = 0;
     for (x = 0; x < this.largeur; x++) {
       for (y = 0; y < this.hauteur; y++) {
-        var caseInstance = new Case(x, y, id);
+        var caseInstance = new Case(x, y, posx, posy, id);
         this.cases.push(caseInstance);
         document.body.appendChild(caseInstance);
         id++;
       }
     }
   };
+  // display all cartes
   this.display = function() {
     for (i = 0; i < (this.largeur*this.hauteur); i++) {
       this.cases[i].draw();
     }
   };
+  // add a carte to plateau
   this.add = function(caseid, img, cout, att, def, titre, desc) {
     this.cases[caseid].carte.add(img, cout, att, def, titre, desc);
     this.cases[caseid].draw();
   };
+  // remove a carte to plateau
   this.remove = function(caseid) {
     this.cases[caseid].carte.remove();
     this.cases[caseid].draw();
   };
+  // clear all cartes
   this.clear = function() {
     for (i = 0; i < (this.largeur*this.hauteur); i++) {
       this.cases[i].carte.remove();
@@ -54,13 +59,13 @@ var mouvementCarte = new Array();
  * Case inherit from Canvas
  * See createAllCases() function.
 */
-function Case(x, y, id) {
+function Case(casex, casey, posx, posy, id) {
   canvasCase = document.createElement('canvas');
   // new properties
-  canvasCase.x = x;
-  canvasCase.y = y;
-  canvasCase.name = "case" + x + "-" + y;
-  canvasCase.carte = new Carte(id);
+  canvasCase.x = casex;
+  canvasCase.y = casex;
+  canvasCase.name = "case" + casex + "-" + casex;
+  canvasCase.carte = new Carte(id, casex, casey);
   // overwrite canvas properties
   canvasCase.ctx = canvasCase.getContext("2d");
   canvasCase.id = id;
@@ -68,8 +73,8 @@ function Case(x, y, id) {
   canvasCase.height = 150;
   canvasCase.style.zIndex = 8;
   canvasCase.style.position = "absolute";
-  canvasCase.style.left = x * canvasCase.width;
-  canvasCase.style.top = y * canvasCase.height;
+  canvasCase.style.left = casex * canvasCase.width + posx;
+  canvasCase.style.top = casey * canvasCase.height + posy;
   canvasCase.style.border = "1px solid grey";
   // overwrite canvas on click method
   canvasCase.onclick = function() {
@@ -80,11 +85,19 @@ function Case(x, y, id) {
       this.draw();
       console.log("Let's move ! " + "\n" + "Array length : " + mouvementCarte.length + "\n" + "case.id : " + this.id + "\n" + "carte.id : " + this.carte.id);
     }
-    else if (mouvementCarte.length != 0) {
+    else if (!this.carte.visible && mouvementCarte.length > 0) {
       this.carte = mouvementCarte.pop();
+      srcx = this.carte.x;
+      srcy = this.carte.y;
       this.carte.visible = true;
+      this.carte.move(this.x, this.y);
       this.draw();
-      console.log("Finish move ! " + "\n" + "Array length : " + mouvementCarte.length + "\n" + "case.id : " + this.id + "\n" + "carte.id : " + this.carte.id);
+      if (mouvementCarte.length == 0) {
+        console.log("Finish move ! " + "\n" + "Array length : " + mouvementCarte.length + "\n" + "case.id : " + this.id + "\n" + "carte.id : " + this.carte.id);
+        console.log(this.toString());console.log(this.carte.toString());
+        therequest = "command=gomove&desx="+this.x+"&desy="+this.y+"&srcx="+srcx+"&srcy="+srcy;
+        goajaxrequete(therequest);
+      }
     }
     else {
       console.log("Impossible to move ! " + "\n" + "Array length : " + mouvementCarte.length + "\n" + "case.id : " + this.id + "\n" + "carte.id : " + this.carte.id);
@@ -123,143 +136,13 @@ function Case(x, y, id) {
   return canvasCase;
 }
 
-/* Constructor for Emplacement.
- * Emplacement inherit from Canvas class.
- * See createAllEmplacements() function.
-*/
-function newEmplacement(x, y) {
-  Emplacement = document.createElement('canvas');
-  Emplacement.x = x;
-  Emplacement.y = y;
-  Emplacement.carte = new Carte(x + y);
-  Emplacement.isCarte = false;
-  Emplacement.ctx = Emplacement.getContext("2d");
-  Emplacement.id = "case" + this.x + "-" + this.y;
-  Emplacement.width = 100;
-  Emplacement.height = 150;
-  Emplacement.style.zIndex = 8;
-  Emplacement.style.position = "absolute";
-  Emplacement.style.left = this.x * Emplacement.width;
-  Emplacement.style.top = this.y * Emplacement.height;
-  Emplacement.style.border = "1px solid grey";
-  Emplacement.isSelect = false;
-  // overwrite canvas onclick method
-  Emplacement.onclick = function() {
-    if (this.isSelect == false) {
-      this.isSelect = true;
-      console.log(this.id + " selected!");
-    }
-    else {
-      this.isSelect = false;
-      console.log(this.id + " unselected!");
-    }
-  };
-  Emplacement.clear = function() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
-  };
-  Emplacement.draw = function() {
-    carreCote = 12;
-    // don't draw if no carte
-    if (this.carteInside()) {
-      this.ctx.beginPath();
-      this.ctx.rect(0, 0, carreCote, carreCote);//haut gauche
-      this.ctx.fillText(this.carte.cout, 0, carreCote-2);
-      this.ctx.fillText(this.carte.titre, carreCote+2, carreCote-2);
-      this.ctx.rect(this.width-carreCote, this.height-carreCote, carreCote, carreCote);//bas droite
-      this.ctx.fillText(this.carte.defense, this.width-carreCote, this.height-2);
-      this.ctx.rect(0, this.height-carreCote, carreCote, carreCote);// bas gauche
-      this.ctx.fillText(this.carte.attaque, 0, this.height-2);
-      this.ctx.fillText(this.carte.description, carreCote/2, this.height-(carreCote+2));
-      this.ctx.drawImage(this.carte.imagej, carreCote+2, carreCote+2, this.width-2*(carreCote+2), this.height-3*(carreCote+2));
-      this.ctx.stroke();
-    }
-  };
-  Emplacement.carteInside = function() {
-    if (this.carte.initialize) {
-      this.isCarte = true;
-      return true;
-    }
-    else {
-      this.isCarte = false;
-      return false;
-    }
-  };
-  Emplacement.toString = function() {
-    emplacementString = "Emplacement :" + "id : " + this.id + " - " + "x : " + this.x + " - " + "y : " + this.y + " - " + "ctx : " + this.ctx + " - " + "isCarte : " + this.isCarte + " - " + "isSelect : " + this.isSelect;
-    return emplacementString + "\n" + this.carte.toString();
-  };
-  return Emplacement;
-}
-/* Same object as Emplacement but can be instantiate using the new operator.
- * See createAllCasesTst() function.
-*/
-function CaseTst(x, y) {
-  this.x = x;
-  this.y = y;
-  this.carte = new Carte(x + y);
-  this.isCarte = false;
-  this.canvas = document.createElement('canvas');
-  this.ctx = this.canvas.getContext("2d");
-  this.canvas.id = "case" + this.x + "-" + this.y;
-  this.canvas.width = 100;
-  this.canvas.height = 150;
-  this.canvas.style.zIndex = 8;
-  this.canvas.style.position = "absolute";
-  this.canvas.style.left = this.x * this.canvas.width;
-  this.canvas.style.top = this.y * this.canvas.height;
-  this.canvas.style.border = "1px solid grey";
-  this.canvas.isSelect = false;
-  // overwrite canvas onclick method
-  this.canvas.onclick = function() {
-    if (this.isSelect == false) {
-      this.isSelect = true;
-      console.log(this.id + " selected!");
-    }
-    else {
-      this.isSelect = false;
-      console.log(this.id + " unselected!");
-    }
-  };
-  this.clear = function() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  };
-  this.draw = function() {
-    carreCote = 12;
-    // don't draw if no carte
-    if (this.carteInside()) {
-      this.ctx.beginPath();
-      this.ctx.rect(0, 0, carreCote, carreCote);//haut gauche
-      this.ctx.fillText(this.carte.cout, 0, carreCote-2);
-      this.ctx.fillText(this.carte.titre, carreCote+2, carreCote-2);
-      this.ctx.rect(this.canvas.width-carreCote, this.canvas.height-carreCote, carreCote, carreCote);//bas droite
-      this.ctx.fillText(this.carte.defense, this.canvas.width-carreCote, this.canvas.height-2);
-      this.ctx.rect(0, this.canvas.height-carreCote, carreCote, carreCote);// bas gauche
-      this.ctx.fillText(this.carte.attaque, 0, this.canvas.height-2);
-      this.ctx.fillText(this.carte.description, carreCote/2, this.canvas.height-(carreCote+2));
-      this.ctx.drawImage(this.carte.imagej, carreCote+2, carreCote+2, this.canvas.width-2*(carreCote+2), this.canvas.height-3*(carreCote+2));
-      this.ctx.stroke();
-    }
-  };
-  this.carteInside = function() {
-    if (this.carte.initialize) {
-      this.isCarte = true;
-      return true;
-    }
-    else {
-      this.isCarte = false;
-      return false;
-    }
-  };
-  this.toString = function() {
-    caseString = "Case :" + "id : " + this.canvas.id + " - " + "x : " + this.x + " - " + "y : " + this.y + " - " + "canvas : " + this.canvas + " - " + "ctx : " + this.ctx + " - " + "isCarte : " + this.isCarte + " - " + "isSelect : " + this.canvas.isSelect;
-    return caseString + "\n" + this.carte.toString();
-  };
-}
 /*
  * Carte prototype.
 */
-function Carte(id) {
+function Carte(id, x, y) {
   this.id = id;
+  this.x = x;
+  this.y = y;
   this.imagej = new Image();
   this.add = function(img, cout, att, def, titre, desc) {
     this.visible = true;
@@ -272,6 +155,8 @@ function Carte(id) {
   };
   this.remove = function() {
     this.id = -1;
+    this.x = -1;
+    this.y = -1;
     this.visible = false;
     this.imagej = new Image();
     this.cout = "";
@@ -280,6 +165,10 @@ function Carte(id) {
     this.titre = "";
     this.description = "";
   };
+  this.move = function(x, y) {
+    this.x = x;
+    this.y = y;
+  }
   this.clone = function() {
     var copy = this.constructor();
     for (var attr in this) {
@@ -300,3 +189,23 @@ Carte.prototype = {
   titre : "",
   description : "",
 };
+
+function goajaxrequete(req) {
+  var xmlhttp;
+  if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  }
+  else { // code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange = function() {
+  if (xmlhttp.readyState==4 && xmlhttp.status == 200 || xmlhttp.status == 304) {
+      //response=xmlhttp.responseText;
+      processajaxresponse(xmlhttp.responseText);
+    }
+  }
+  // requete treatment
+  xmlhttp.open("POST", "http://localhost:8080/?"+req, true);
+  //xmlhttp.setRequestHeader("Content-type", "application/html");//not to localhost
+  xmlhttp.send();
+}
